58b64ca03e6c570efb9336f5ae530cfca9c1ec5b
Craig Butler <craigbutler123456@gmail.com>
2023-08-20 13:11:43 -0600

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by user on 2023-08-20.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
