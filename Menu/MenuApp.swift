e479bd4a71eff4d4dec15a1f6027bc04c0f97cbb
Craig Butler <craigbutler123456@gmail.com>
2023-08-23 06:07:24 -0600

    Create a instance of menuItem

--- Menu/MenuApp.swift
+++ Menu/MenuApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct MenuApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MenuView()
+                
+               
+                
         }
     }
 }
---
+++ Menu/Model /MenuItem .swift
@@ -0,0 +1,16 @@
+//
+//  MenuItem .swift
+//  Menu
+//
+//  Created by user on 2023-08-21.
+//
+
+import Foundation
+
+struct MenuItem {
+    
+    var name : String
+    var price : String
+    var imageName : String 
+    
+}
