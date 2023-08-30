31f043593543fd5cdcb73867c4b97748f8f69e59
Craig Butler <craigbutler123456@gmail.com>
2023-08-29 18:59:16 -0600

    - create a state value for menuItems - create a dataService instance of DataService() - add OnAppear modifier to List that calls dataService.getdate()

--- Menu/View/MenuView.swift
+++ Menu/View/MenuView.swift
@@ -9,33 +9,41 @@ import SwiftUI
 
 struct MenuView: View {
     
+    @State var menuItems: [MenuItem] = [MenuItem]()
+    var dataService = DataService()
+    
     var body: some View {
         
-        let menuItems: [MenuItem] = [MenuItem]()
-        
+
         List (menuItems) { items in
             
-                HStack {
-                    
-                    Image (items.imageName)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 50)
-                        .cornerRadius(10)
-                        
-                    Text (items.name)
-                        .bold()
-                    
-                    Spacer()
-                    
-                    Text ("$" + items.price)
-                  
-                }
-                .listRowSeparator(.hidden)
-                .listRowBackground(Color(.brown).opacity(0.1))
+            HStack {
+                
+                Image (items.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 50)
+                    .cornerRadius(10)
+                
+                Text (items.name)
+                    .bold()
+                
+                Spacer()
+                
+                Text ("$" + items.price)
+                
+            }
+            .listRowSeparator(.hidden)
+            .listRowBackground(Color(.brown).opacity(0.1)
+            )
+            
         }
         .listStyle(.plain)
-        
+        .onAppear {
+            //Call for the data
+            menuItems =  dataService.getData()
+        }
+      
     }
 }
 
